93b8dac71102073687e5792c4085e4b17b705973
Alexander Keusch <alex@keusch.one>
2024-04-22 13:45:54 +0200

    breast evaluation

--- exercise-1/evaluation/evaluator.py
+++ exercise-1/evaluation/evaluator.py
@@ -149,10 +149,10 @@ def __extract_fields(data_sets):
 #     },
 # }
 
-def draw_diagram2(evaluation_results, x_axis="alpha", y_axis="accuracy", figsize=(10, 6), title=None):
+def draw_diagram2(evaluation_results, x_axis="alpha", y_axis="accuracy", figsize=(10, 6), title=None, logaritmic=False, line=True):
     plt.figure(figsize=figsize)
 
-
+    is_str = False
     for dataset_name, results in evaluation_results.items():
 
         x_values = [metrics[x_axis] for metrics in results.values()]
@@ -160,6 +160,7 @@ def draw_diagram2(evaluation_results, x_axis="alpha", y_axis="accuracy", figsize
         if isinstance(x_values[0], (int, float, complex)):
             plt.plot(x_values, y_values, marker='o', label=dataset_name)
         else:
+            is_str = True
             # Extract key-value pairs and sort based on the x_metric lexicographically
             items = [(config[x_axis], config[y_axis]) for config in results.values()]
             items.sort()  # This sorts tuples lexicographically by default
@@ -170,23 +171,32 @@ def draw_diagram2(evaluation_results, x_axis="alpha", y_axis="accuracy", figsize
             # Convert tuples to string labels if necessary
             x_labels = [str(x) if isinstance(x, tuple) else x for x in x_values]
 
-            plt.plot(x_labels, y_values, marker='o', label=dataset_name)
+            if line:
+                plt.plot(x_labels, y_values, marker='o', label=dataset_name)
+            else:
+                plt.scatter(x_labels, y_values, marker='o', label=dataset_name)
 
 
     if title is not None:
         plt.title(title)
     else:
         plt.title(y_axis.capitalize())
+    # make x-axis logarithmic
+    if logaritmic:
+        plt.xscale('log')
+
     plt.xlabel(x_axis.capitalize())
     plt.ylabel(y_axis.capitalize())
+    if is_str:
+        plt.xticks(rotation=45)
     plt.legend()
     plt.tight_layout()
     plt.show()
 
-def draw_diagram2_list(evaluation_results, x_axis="alpha", y_axis=["accuracy", "precision"], figsize=(10, 6), title=None):
+def draw_diagram2_list(evaluation_results, x_axis="alpha", y_axis=["accuracy", "precision"], figsize=(10, 6), title=None, logaritmic=False, line=True):
 
     for i, y in enumerate(y_axis):
-        draw_diagram2(evaluation_results, x_axis, y, figsize, title[i] if title is not None else None)
+        draw_diagram2(evaluation_results, x_axis, y, figsize, title[i] if title is not None else None, logaritmic, line)
 
 
 def draw_diagrams(evaluation_results):
