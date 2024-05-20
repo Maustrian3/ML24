f214843b2d8243e50248eba5270cd3c57b806c48
Michael Trauner <e12019868@student.tuwien.ac.at>
2024-05-20 23:33:31 +0200

    [Gradient] Basic implementation with any number of features

--- exercise-2/gradient.py
+++ exercise-2/gradient.py
@@ -2,9 +2,10 @@ import numpy as np
 import pandas as pd
 from scipy.io import arff
 from sklearn.datasets import fetch_california_housing
-from sklearn.linear_model import SGDRegressor
+from sklearn.linear_model import SGDRegressor, LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
 
 
 # def gradient_descent(X, y, alpha, epochs):
@@ -16,54 +17,54 @@ from sklearn.metrics import mean_squared_error
 # The derivatives in respect to the known features
 # Update the parameters with the new gradient
 
-# TODO step1: implement basic with 1 feature -> easier to understand
 # TODO stepX: Also programmatically derive loss functions? Is this needed? Or just use 2-3 different ones and calc them beforehand.
 class MySGDRegressor:
     # Loss Function: Sum of Squared Residuals
     # sum_squared_residuals = (observed - predicted)^2 = (observed - (intercept + slope * cur_predicted))^2
 
-    def __init__(self, max_iter=1000, alpha=0.0001, intercept=0, slope=1):
-        self.slope = slope
-        self.intercept = intercept
+    def __init__(self, max_iter=1000, alpha=0.0001, intercept=0):
         self.max_iter = max_iter
         self.alpha = alpha
-
-    def init_params(self, intercept=0, slope=1):
         self.intercept = intercept
-        self.slope = slope
+        self.coefficients = None
+
+    def __init_params(self, n_features):
+        self.intercept = 0
+        self.coefficients = np.zeros(n_features)
 
     def __update_gradients(self, X, y):
         # Calculate new slope and intercept
         # Loss Function: Sum of Squared Residuals
         # Calculate derivative in respect to the slope
         # derivative(sum_squared_residuals) = -2 * (observed - (intercept + slope * cur_predicted))
+
+        n_samples, n_features = X.shape
         intercept_gradient = 0
-        slope_gradient = 0
-        for i in range(len(X)):  # For each sample do:
-            # calc the value of the current sample
-            # TODO change to handle more than 1 feature
-            intercept_gradient += -2 * (y[i] - (self.intercept + self.slope * X[i]))
-            slope_gradient += -2 * X[i] * (y[i] - (self.intercept + self.slope * X[i]))
-            # sum up
-        # Resulting in a 'slope' for the step size calculation of the slope
-        # Plug in into step size formula for the slope
-        step_size_intercept = (intercept_gradient / len(X)) * self.alpha
-        step_size_slope = (slope_gradient / len(X)) * self.alpha
+        coefficients_gradient = 0
+
+        predictions = self.intercept + X.dot(self.coefficients)
+        errors = y - predictions
+
+        intercept_gradient += -2 * np.sum(errors) / n_samples
+        coefficients_gradient += -2 * X.T.dot(errors) / n_samples
+
+        step_size_intercept = intercept_gradient * self.alpha
+        step_size_slope = coefficients_gradient * self.alpha
 
         # Calc new slope/intercept values
         self.intercept = self.intercept - step_size_intercept
-        self.slope = self.slope - step_size_slope
+        self.coefficients = self.coefficients - step_size_slope
 
     def fit(self, X, y):
+        self.__init_params(X.shape[1])
         for _ in range(self.max_iter):
             self.__update_gradients(X, y)
 
     def predict(self, X):
-        return self.intercept + self.slope * np.array(X)
+        return self.intercept + np.dot(X, self.coefficients)
 
 
 def main():
-    # TODO use specified dataset
     # data = arff.loadarff('./datasets/black_friday.arff')
     # df = pd.DataFrame(data[0])
 
@@ -71,29 +72,40 @@ def main():
     X = california.data
     y = california.target
 
-    # TODO make sure X has only 1 feature
-    X = X[:, 0]
-    X = X.reshape((-1, 1))
+    scaler = StandardScaler()
+    X_normalized = scaler.fit_transform(X)
 
     # splitting dataset into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X_normalized, y, test_size=0.2, random_state=42)
 
-    max_iter = 200
-    alpha = 0.001
+    max_iter = 1000
+    alpha = 0.0001
 
-    sgd_regressor = SGDRegressor(max_iter=max_iter, alpha=alpha, learning_rate='invscaling', random_state=42)
+    # Least squares regression model
+    ls_model = LinearRegression()
+    ls_model.fit(X_train, y_train)
+    ls_y_pred = ls_model.predict(X_test)
+
+    sgd_regressor = SGDRegressor(max_iter=max_iter, alpha=alpha, eta0=alpha,
+                                 loss='squared_error',
+                                 learning_rate='constant', tol=None,
+                                 shuffle=False,
+                                 penalty=None
+                                 )
     sgd_regressor.fit(X_train, y_train)
-    y_pred = sgd_regressor.predict(X_test)
+    sgd_y_pred = sgd_regressor.predict(X_test)
 
     my_sgd_regressor = MySGDRegressor(max_iter=max_iter, alpha=alpha)
     my_sgd_regressor.fit(X_train, y_train)
     my_y_pred = my_sgd_regressor.predict(X_test)
 
     # Calc mean squared error for evaluation
-    mse = mean_squared_error(y_test, y_pred)
+    ls_mse = mean_squared_error(y_test, ls_y_pred)
+    sgd_mse = mean_squared_error(y_test, sgd_y_pred)
     my_mse = mean_squared_error(y_test, my_y_pred)
-    print(mse)
-    print(my_mse)
+    print("sklearn ls regressor: ", ls_mse)
+    print("sklearn sgd regressor: ", sgd_mse)
+    print("my regressor: ", my_mse)
 
 
 if __name__ == '__main__':
